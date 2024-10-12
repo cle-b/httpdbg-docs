8025d599d9f03140de743f1b35eea8d50c38d522
cle-b <cle@tictac.pm>
2024-10-12 10:44:57 +0200

    pytest fixture ans tags in IU

--- examples/test_demo.py
+++ examples/test_demo.py
@@ -2,15 +2,22 @@ import pytest
 import requests
 
 
+@pytest.fixture(scope="session", autouse=True)
+def get_in_a_fixture():
+    requests.get("https://httpbin.org/get/session/setup")
+    yield
+    requests.get("https://httpbin.org/get/session/teardown")
+
+
 @pytest.fixture()
-def a_post():
+def post_in_a_fixture():
     return requests.post("https://httpbin.org/post", json={"demo": "in-fixture"})
 
 
-def test_post(a_post):
-    assert a_post.status_code == 200
+def test_post(post_in_a_fixture):
+    assert post_in_a_fixture.status_code == 200
     assert (
-        requests.post("https://httpbin.org/post", json={"demo": "in-post"}).status_code
+        requests.post("https://httpbin.org/post", json={"demo": "in-test"}).status_code
         == 200
     )
 
